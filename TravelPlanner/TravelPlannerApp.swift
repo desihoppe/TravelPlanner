818acbb8e2424f46b0e889e0b60daa8f4e2c0129
Desiree Hoppe <desihoppe@gmail.com>
2024-11-06 17:07:50 -0300

    Initial commit: Create TravelPlanner project

---
+++ TravelPlanner/TravelPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelPlannerApp.swift
+//  TravelPlanner
+//
+//  Created by Desiree Hoppe on 31/10/24.
+//
+
+import SwiftUI
+
+@main
+struct TravelPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
